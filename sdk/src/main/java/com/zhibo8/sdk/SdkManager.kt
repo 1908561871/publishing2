112e7fa64781cf44fd6a4009661464b46ec0d08b
zhangweibo <1908561871@qq.com>
2022-10-13 16:01:41 +0800

    init

---
+++ sdk/src/main/java/com/zhibo8/sdk/SdkManager.kt
@@ -0,0 +1,14 @@
+package com.zhibo8.sdk
+
+/**
+ * @author : ZhangWeiBo
+ * date : 2022/10/13
+ * email : 1908561871@qq.com
+ * description : TODO
+ */
+class SdkManager {
+
+    fun  init(){
+
+    }
+}
